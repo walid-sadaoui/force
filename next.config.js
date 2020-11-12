331176f54e8bd69d02af977d2ac2c7e460598b54
Christopher Pappas <damassi.pappas@gmail.com>
2020-11-12 14:43:33 -0800

    fix images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
     METAPHYSICS_ENDPOINT: process.env.METAPHYSICS_ENDPOINT,
     GEMINI_CLOUDFRONT_URL: process.env.GEMINI_CLOUDFRONT_URL,
   },
+  publicRuntimeConfig: {
+    METAPHYSICS_ENDPOINT: process.env.METAPHYSICS_ENDPOINT,
+    GEMINI_CLOUDFRONT_URL: process.env.GEMINI_CLOUDFRONT_URL,
+  },
   typescript: {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
--- src/v2/Utils/getENV.tsx
+++ src/v2/Utils/getENV.tsx
@@ -1,11 +1,17 @@
 import { GlobalData, data as sd } from "sharify"
+import getConfig from "next/config"
+const { publicRuntimeConfig } = getConfig()
 
 export function getENV(ENV_VAR: keyof GlobalData) {
   let envVar
   if (typeof window === "undefined") {
     envVar = process.env[ENV_VAR]
   } else {
-    envVar = sd[ENV_VAR]
+    if (publicRuntimeConfig[ENV_VAR]) {
+      envVar = publicRuntimeConfig[ENV_VAR]
+    } else {
+      envVar = sd[ENV_VAR]
+    }
   }
 
   return envVar
